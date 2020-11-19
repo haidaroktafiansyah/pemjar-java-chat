f794b5a36608e41d918819f5497dd97e806aeb13
dharmayudistira <dharmayudistira2000@gmail.com>
2020-11-19 10:37:52 +0700

    init project

---
+++ Chatbot Application/src/chatbot/application/utils/ChatbotConstant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chatbot.application.utils;
+
+/**
+ *
+ * @author dharma
+ */
+public class ChatbotConstant {
+    public static final int PORT_NUMBER = 1201;
+    public static final String LOCALHOST = "127.0.0.1";
+}
